a664dec7d84c612e2571aca5599744c8cbd0a509
marypaskhaver <45778203+marypaskhaver@users.noreply.github.com>
2019-11-27 21:43:56 -0500

    Make separate class to generate magician names

--- ViewController.swift
+++ ViewController.swift
@@ -73,87 +73,30 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
         return randomWord.prefix(1).uppercased() + randomWord.lowercased().dropFirst()
     }
     
-    public func replaceLetters() {
-        
-        let transitionTable: [String : [String]] = [
-            "a": ["a", "ae", "ai"],
-            "b": ["b", "v"],
-            "c": ["c", "k"],
-            "e": ["ae", "e", "i"],
-            "f": ["f", "ph"],
-            "g": ["g", "j"],
-            "j": ["j", "dg"],
-            "q": ["q", "kyu"],
-            "s": ["s", "es", "ss"],
-            "t": ["t", "ti"],
-            "u": ["u", "yu"],
-            "v": ["v", "vi", "vee"],
-            "y": ["y", "i", "ie"],
-            "z": ["z", "zee", "zi", "zed"]
-        ]
-    
-        for letterChar in usersName {
-            let isLetterCapitalized: Bool = letterChar.isUppercase
-            let letter = String(letterChar).lowercased()
-            var randomReplacementLetter: String = letter
-                
-            if transitionTable[letter] != nil {
-                let randomInt = Int.random(in: 0..<(transitionTable[letter]?.count ?? 1))
-                randomReplacementLetter = transitionTable[letter]![randomInt]
-            }
-            
-            if isLetterCapitalized {
-                randomReplacementLetter = randomReplacementLetter.uppercased()
-            }
-            
-            usersName = usersName.stringByReplacingFirstOccurrenceOfString(target: String(letterChar), withString: randomReplacementLetter)
-        }
-    }
-    
     public func generateMagicianName() {
-        replaceLetters()
-        usersName += magicSuffix().lowercased()
+        let mg = MagicianNameGenerator()        
+        usersName = mg.generate(forName: usersName)
         pseudonymLabel.text = usersName
-    }
-    
-    public func magicSuffix() -> String {
-        var randomWord = ""
-        
-        if let path = Bundle.main.path(forResource: "magicSuffixes", ofType: "txt") {
-            do {
-                let data = try String(contentsOfFile: path, encoding: .utf8)
-                let lines = data.components(separatedBy: .newlines)
-
-                randomWord = lines[numericCast(arc4random_uniform(numericCast(lines.count)))]
-
-            } catch {
-                print(error)
-            }
-        }
-
-        // Capitalizes first letter of each word
-        return randomWord.prefix(1).uppercased() + randomWord.lowercased().dropFirst()
     }
     
     public func generateWarriorName() {
          pseudonymLabel.text = usersName + " the Bar"
     }
     
-    
     public func numberOfComponents(in pickerView: UIPickerView) -> Int{
         return 1
-        
     }
     
     public func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int{
         return list.count
     }
     
-    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String {
         self.view.endEditing(true)
         listItem = list[row]
         return list[row]
     }
+    
 }
 
 extension String
@@ -164,6 +107,7 @@ extension String
         if let range = self.range(of: target) {
             return self.replacingCharacters(in: range, with: replaceString)
         }
+        
         return self
     }
 }
